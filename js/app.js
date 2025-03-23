5bcbfd08b14e558167b6884dcaecc3cda8cdd9a3
fatimajavadzade <fatimajavadzde@gmail.com>
2025-03-24 00:57:29 +0400

    sidebar added

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const sidebar = document.getElementById('sidebar')
+const openSidebar = document.getElementById('openSidebar')
+const closeSidebar = document.getElementById('closeSidebar')
+
+document.getElementById("openSidebar").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.remove("hidden");
+});
+
+document.getElementById("closeSidebar").addEventListener("click", function() {
+    document.getElementById("sidebar").classList.add("hidden");
+});
+
+
+
+
+
+
